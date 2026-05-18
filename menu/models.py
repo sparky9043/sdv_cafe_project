c6f060894b71a3c0756856ffeb3438ceb0af2590
Se Joon Park <sparky9043@gmail.com>
2026-05-18 15:06:46 +0900

    feat: make category plural to categories in admin site

--- menu/models.py
+++ menu/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.name
 
