33bdfa0809e8e22d4b45a59d3017e27c80bcedcc
Se Joon Park <sparky9043@gmail.com>
2026-05-18 15:04:27 +0900

    feat: make Category and MenuItem models

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    category = models.ForeignKey(
+        Category,
+        related_name="items",
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"[{self.category}] {self.name}"
