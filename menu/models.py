eb7855daaa8005d85db40b65410c68bc5c161995
Se Joon Park <sparky9043@gmail.com>
2026-05-21 14:31:25 +0900

    feat: add description to models and show item list

--- menu/models.py
+++ menu/models.py
@@ -20,6 +20,7 @@ class MenuItem(models.Model):
     )
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=255, blank=True)
 
     def __str__(self):
         return f"[{self.category}] {self.name}"
--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import MenuItem
+
 
 # Create your views here.
-
-
 class MenuHomeView(TemplateView):
     template_name = "menu/home.html"
+
+
+class ItemListView(ListView):
+    model = MenuItem
+    context_object_name = "items"
+    template_name = "menu/item_list.html"
+
+    def get_queryset(self):
+        return super().get_queryset().order_by("category", "price")
