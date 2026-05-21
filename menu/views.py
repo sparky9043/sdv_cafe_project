7c32c60458050eb54762e87125e3503a4e578308
Se Joon Park <sparky9043@gmail.com>
2026-05-21 15:03:48 +0900

    feat: use context to show active link

--- menu/views.py
+++ menu/views.py
@@ -7,6 +7,11 @@ from .models import MenuItem
 class MenuHomeView(TemplateView):
     template_name = "menu/home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["active_page"] = "home"
+        return context
+
 
 class ItemListView(ListView):
     model = MenuItem
@@ -15,3 +20,8 @@ class ItemListView(ListView):
 
     def get_queryset(self):
         return super().get_queryset().order_by("category", "price")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["active_page"] = "item_list"
+        return context
