bd11e58b631febaf21c813da4cb88a4c346fce88
Se Joon Park <sparky9043@gmail.com>
2026-05-21 15:36:58 +0900

    feat: create OrderItem and Size models and register to admin

--- order/models.py
+++ order/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
+class Size(models.Model):
+    type = models.CharField(
+        max_length=10,
+        choices=[
+            ("small", "small"),
+            ("medium", "medium"),
+            ("large", "large"),
+        ],
+    )
+
+
 # Create your models here.
+class OrderItem(models.Model):
+    menu_item = models.ForeignKey(
+        "menu.MenuItem",
+        related_name="order_items",
+        on_delete=models.CASCADE,
+    )
+    quantity = models.IntegerField()
+    size = models.ForeignKey(
+        Size,
+        related_name="order_items",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+
+    def __str__(self):
+        return f"{self.menu_item}"
